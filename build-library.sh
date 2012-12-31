c9015037e81de37fd491f6e4e5190553c2bb761d
Michael L Heuer <heuermh@acm.org>
2012-12-31 17:31:30 -0600

    animation --> frames, commit 3 of 3

--- build-library.sh
+++ build-library.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir animation
-mkdir animation/library
-cp COPYING animation
-cp README animation
-cp library.properties animation
-cp -R src animation
-cp -R examples animation
+mkdir frames
+mkdir frames/library
+cp COPYING frames
+cp README frames
+cp library.properties frames
+cp -R src frames
+cp -R examples frames
 cd src
-javac -source 1.6 -target 1.6 -classpath "../lib/processing-core-1.5.1.jar" org/dishevelled/processing/animation/*.java
-jar cvf ../animation/library/animation.jar org/dishevelled/processing/animation/*.class
+javac -source 1.6 -target 1.6 -classpath "../lib/processing-core-1.5.1.jar" org/dishevelled/processing/frames/*.java
+jar cvf ../frames/library/frames.jar org/dishevelled/processing/frames/*.class
 cd ..
