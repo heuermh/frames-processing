998e53bed57d5c94f56ec12d0a087c051de2046d
Michael L Heuer <heuermh@acm.org>
2019-10-24 10:21:28 -0500

    update processing-core to 3.3.6

--- build-library.sh
+++ build-library.sh
@@ -8,6 +8,6 @@ cp library.properties frames
 cp -R src frames
 cp -R examples frames
 cd src
-javac -source 1.6 -target 1.6 -classpath "../lib/processing-core-1.5.1.jar" org/dishevelled/processing/frames/*.java
+javac -source 1.8 -target 1.8 -classpath "../lib/processing-core-3.3.6.jar" org/dishevelled/processing/frames/*.java
 jar cvf ../frames/library/frames.jar org/dishevelled/processing/frames/*.class
 cd ..
