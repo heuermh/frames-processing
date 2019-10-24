0012b56a439f7956fb98d1b0c7565f615372e1b9
Michael L Heuer <heuermh@acm.org>
2019-10-24 10:51:40 -0500

    update processing-core dependency version to 3.5.3

--- build-library.sh
+++ build-library.sh
@@ -8,6 +8,6 @@ cp library.properties frames
 cp -R src frames
 cp -R examples frames
 cd src
-javac -source 1.8 -target 1.8 -classpath "../lib/processing-core-3.3.6.jar" org/dishevelled/processing/frames/*.java
+javac -source 1.8 -target 1.8 -classpath "../lib/processing-core-3.5.3.jar" org/dishevelled/processing/frames/*.java
 jar cvf ../frames/library/frames.jar org/dishevelled/processing/frames/*.class
 cd ..
